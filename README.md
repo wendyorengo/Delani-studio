98df1e308ed2264f94bb96fc246f16c1084a7d96
wendyorengo1 <orengowendy594@gmail.com>
2019-09-30 11:01:28 +0300

    add a README.md

---
+++ README.md
@@ -0,0 +1,29 @@
+# Delani-studio
+
+# Author
+. Wendy orengo
+
+# Description
+This project is a web application for Delani-studios that shows details about the services it offers, has information about the studio, a portfolio section and a form that enables users to send their messages & concerns to Delani-studios.
+
+# Live page
+- https://wendyorengo.github.io/Delani-studio/.
+
+# Installation
+. open terminal {ctrl+alt+T}
+. git clone 
+. git cd Deskop,moringa-school-projects,Delani-studio.
+. code .or atom . depending on the text editor.
+
+# technologies Used
+. HTML
+. CSS
+. javascript
+. jQuery
+
+# contact information
+. For any inquiries contact me through {orengowendy594@gmail.com}
+
+# License
+- MIT license
+- copyright(c)2019
--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,79 @@
 $(document).ready(function () {
-  $(".image-hover").click(function () {
-    $(".p-show").toggle();
-  });
-})
 
-$(document).ready(function(){
-  $(",portfolio").hover(function () {
-    $("")
-  }
-}
+$(".image-hover").click(function(){
+  $(".pata").toggle();
+  $(".toggler").toggle();
+});
+
+$(".imager").click(function(){
+  $(".pera").toggle();
+  $(".developmenter").toggle();
+});
+
+$(".seer").click(function(){
+  $(".para").toggle();
+  $(".productor").toggle();
+});
+
+
+$(".one").hover(function () {
+  $("#overlay1").show();
+})
+$(".one").mouseleave(function () {
+  $("#overlay1").hide();
+
+});
+
+$(".two").hover(function () {
+  $("#overlay2").show();
+})
+$(".two").mouseleave(function () {
+  $("#overlay2").hide();
+
+});
+
+$(".three").hover(function () {
+  $("#overlay3").show();
+})
+$(".three").mouseleave(function () {
+  $("#overlay3").hide();
+
+});
+$(".four").hover(function () {
+  $("#overlay4").show();
+})
+$(".four").mouseleave(function () {
+  $("#overlay4").hide();
+
+});
+$(".five").hover(function () {
+  $("#overlay5").show();
+})
+$(".five").mouseleave(function () {
+  $("#overlay5").hide();
+
+});
+$(".six").hover(function () {
+  $("#overlay6").show();
+})
+$(".six").mouseleave(function () {
+  $("#overlay6").hide();
+
+});
+$(".seven").hover(function () {
+  $("#overlay7").show();
+})
+$(".seven").mouseleave(function () {
+  $("#overlay7").hide();
+
+});
+$(".eight").hover(function () {
+  $("#overlay8").show();
+})
+$(".eight").mouseleave(function () {
+  $("#overlay8").hide();
+
+});
+});
+
+
